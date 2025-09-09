e66eb946e00b3c1cc15f515d3524ac60ffdb46de
egor4ikaa <egor7ananiev@gmail.com>
2025-09-09 16:47:23 +0300

    Update print.py

--- print.py
+++ print.py
@@ -10,43 +10,32 @@ BOT_TOKEN = '8242768529:AAEJtAIvGKvIBSEI7IYTHmx-K5MScSgtX3I'
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-async def process_start_command(message: Message):
-    await message.answer("Хэй, меня зовут Эхо-Бот! \nнапиши мне что-нибудь")
-
-async def process_help_command(message: Message):
-    await message.answer('Напиши что-нибудь, отправлю в ответ!')
-
+@dp.message(Command(commands='contact'))
 async def process_contact_command(message: Message):
     await message.answer('Не так быстро, мы ещё знакомы!')
 
-async def send_photo_echo(message: Message):
-    await message.reply_photo(message.photo[0].file_id)
+@dp.message(Command(commands='start'))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
-async def send_audio_echo(message: Message):
-    await message.reply_audio(message.audio.file_id)
 
-async def send_voice_echo(message: Message):
-    await message.reply_voice(message.voice.file_id)
+@dp.message(Command(commands='help'))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
 
-async def send_document_echo(message: Message):
-    await message.reply_document(message.document.file_id)
-
-async def send_sticker_echo(message: Message):
-    await message.reply_sticker(message.sticker.file_id)
-
+@dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+                 'методом send_copy'
+        )
 
-dp.message.register(process_start_command, Command(commands='start'))
-dp.message.register(process_help_command, Command(commands='help'))
-dp.message.register(process_contact_command, Command(commands='contact'))
-dp.message.register(send_photo_echo, F.photo)
-dp.message.register(send_audio_echo, F.audio)
-dp.message.register(send_voice_echo, F.voice)
-dp.message.register(send_document_echo, F.document)
-dp.message.register(send_sticker_echo, F.sticker)
-
-dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
